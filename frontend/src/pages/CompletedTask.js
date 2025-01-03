56743fb3ec894ff26d09fb8b32edfceb52c712d0
anjalirathor622 <anjalirathor622@gmail.com>
2025-01-03 14:18:36 +0530

    completed ttask page done

--- frontend/src/pages/CompletedTask.js
+++ frontend/src/pages/CompletedTask.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Cards from '../components/Home/Cards'
+import axios from 'axios'
 
 const CompletedTask = () => {
+  const [data,setData] = useState()
+  const headers = {
+		id: localStorage.getItem("id"),
+		authorization: `Bearer ${localStorage.getItem("token")}`
+	}
+	useEffect(() => {
+		const fetch = async () => {
+			const response = await axios.get(
+				"http://localhost:1000/task/completeTasks",
+				{ headers }
+			)
+      console.log(response)
+			setData(response.data.data)
+		}
+		fetch()
+	},[])
   return (
-   <Cards home={false}/>
+   <Cards home={false} data={data}/>
   )
 }
 
--- frontend/src/pages/Pending.js
+++ frontend/src/pages/Pending.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Cards from '../components/Home/Cards'
+import axios from 'axios'
 
 const Pending = () => {
+  const [data,setData] = useState()
+  const headers = {
+		id: localStorage.getItem("id"),
+		authorization: `Bearer ${localStorage.getItem("token")}`
+	}
+	useEffect(() => {
+		const fetch = async () => {
+			const response = await axios.get(
+				"http://localhost:1000/task/pendingTasks",
+				{ headers }
+			)
+      console.log(response)
+			setData(response.data.data)
+		}
+		fetch()
+	},[])
   return (
-    <Cards home={false}/>
+    <Cards home={false} data={data}/>
    )
 }
 
