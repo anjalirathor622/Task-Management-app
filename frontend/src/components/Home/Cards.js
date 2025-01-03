30c0f327ca1e5720a31b92ccdfb961c161ecb0b7
anjalirathor622 <anjalirathor622@gmail.com>
2025-01-03 11:29:03 +0530

    logout done

--- frontend/src/components/Home/Cards.js
+++ frontend/src/components/Home/Cards.js
@@ -4,39 +4,7 @@ import { MdDeleteForever } from "react-icons/md";
 import { FaRegEdit } from "react-icons/fa";
 import { MdOutlineAddCircle } from "react-icons/md";
 
-const Cards = ({home, setInputDiv}) => {
-	const data = [
-		{
-			title: "ajsnufuehfjen",
-			desc: "Derivatives market data shows FIIs net sold 78,868 contracts of Nifty futures in the 4 trading sessions while ",
-			dueDate: "12/2/24",
-            status:"Complete"
-		},
-		{
-			title: "ajsnufuehfjen",
-			desc: "OI rose by 74,992 contracts; thus implying build-up of short positions in Nifty.",
-			dueDate: "12/2/24",
-            status:"In Progress"
-		},
-		{
-			title: "ajsnufuehfjen",
-			desc: "Markets Today LIVE: Benchmark Indian equity indices BSE Sensex and Nifty50 opened higher on Thursday",
-			dueDate: "12/2/24",
-            status:"Pending..."
-		},
-		{
-			title: "ajsnufuehfjen",
-			desc: "amid mixed global cues as most major global markets returned from the New Year's Day break",
-			dueDate: "12/2/24",
-            status:"Complete"
-		},
-		{
-			title: "ajsnufuehfjen",
-			desc: "amid mixed global cues as most major global markets returned from the New Year's Day break",
-			dueDate: "12/2/24",
-            status:"Pending..."
-		}
-	]
+const Cards = ({home, setInputDiv,data}) => {
 	return (
 		<div className="grid grid-cols-4 gap-4 p-4">
 			{data &&
@@ -47,13 +15,13 @@ const Cards = ({home, setInputDiv}) => {
 								{item.title}
 							</h3>
 							<p className="text-gray-300 pt-2 my-2">
-								{item.desc}
+								{item.description}
 							</p>
 						</div>
 							<h4 className="pb-2">dueDate : {item.dueDate}</h4>
 						<div className="w-full flex flex-row flex-center">
-							<button className={`${item.status==="Complete"?"bg-green-600":"bg-red-500"} p-2 rounded-md w-3/6`}>
-								{item.status}
+							<button className={`${item.completed===true?"bg-green-600":"bg-red-500"} p-2 rounded-md w-3/6`}>
+								{item.completed===true? "completed":"pending..."}
 							</button>
 							<div className="w-3/6 flex justify-around text-2xl">
 								<button><GrView /></button>
--- frontend/src/components/Home/Sidebar.js
+++ frontend/src/components/Home/Sidebar.js
@@ -1,42 +1,78 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { CgNotes } from "react-icons/cg"
 import { MdOutlineRemoveDone } from "react-icons/md"
 import { IoMdDoneAll } from "react-icons/io"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { useDispatch } from "react-redux"
+import { authActions } from "../../store/auth"
+import axios from "axios"
 const Sidebar = () => {
-	const data = [
+	const [data, setData] = useState()
+	const dispatch = useDispatch()
+	const navigate = useNavigate()
+	const SidebarData = [
 		{
 			title: "All task",
 			icon: <CgNotes />,
-      link:"/"
+			link: "/"
 		},
 		{
 			title: "Compeleted task",
 			icon: <IoMdDoneAll />,
-      link:"/completedTask"
+			link: "/completedTask"
 		},
 		{
 			title: "Pending task",
 			icon: <MdOutlineRemoveDone />,
-      link:"/pendingTask"
+			link: "/pendingTask"
 		}
 	]
+	const logout = () => {
+		dispatch(authActions.logout())
+		localStorage.clear("id")
+		localStorage.clear("token")
+		navigate("/signup")
+	}
+	const headers = {
+		id: localStorage.getItem("id"),
+		authorization: `Bearer ${localStorage.getItem("token")}`
+	}
+	useEffect(() => {
+		const fetch = async () => {
+			const response = await axios.get(
+				"http://localhost:1000/task/AllTasks",
+				{ headers }
+			)
+			setData(response.data.data)
+		}
+		fetch()
+	}, [])
 	return (
 		<>
+			{data && (
+				<div>
+					<h2 className="text-xl font-semibold">{data.username}</h2>
+					<h4 className="mb-2 text-gray-400">{data.email}</h4>
+					<hr />
+				</div>
+			)}
+
 			<div>
-				<h2 className="text-xl font-semibold">the code master</h2>
-				<h4 className="mb-2 text-gray-400">email</h4>
-				<hr />
-			</div>
-			<div>
-				{data.map((items, i) => (
-					<Link to={items.link} key={i} className="my-2 flex items-center hover:bg-gray-600 p-2 rounded transition-all duration-300">
+				{SidebarData.map((items, i) => (
+					<Link
+						to={items.link}
+						key={i}
+						className="my-2 flex items-center hover:bg-gray-600 p-2 rounded transition-all duration-300"
+					>
 						{items.icon} &nbsp; {items.title}
 					</Link>
 				))}
 			</div>
 			<div>
-				<button className="bg-gray-600 w-full p-2 rounded">
+				<button
+					className="bg-gray-600 w-full p-2 rounded"
+					onClick={logout}
+				>
 					Log Out
 				</button>
 			</div>
